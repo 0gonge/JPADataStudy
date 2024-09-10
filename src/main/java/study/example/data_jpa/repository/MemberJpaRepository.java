05c22eaad7dece8e8e7ce9da6efd9c4f9938aa76
0gonge <Challengef0802@gmail.com>
2024-09-11 04:55:19 +0900

    MemberJpaRepository 기본 CRUD구현

--- src/main/java/study/example/data_jpa/repository/MemberJpaRepository.java
+++ src/main/java/study/example/data_jpa/repository/MemberJpaRepository.java
@@ -5,6 +5,9 @@ import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 import study.example.data_jpa.entity.Member;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public class MemberJpaRepository {
     @PersistenceContext
@@ -13,6 +16,26 @@ public class MemberJpaRepository {
     public Member save(Member member) {
         em.persist(member);
         return member;
+    }
+
+    public  void delete(Member member)  {
+        em.remove(member);
+    } //단건 조회
+
+    public List<Member> findAll() {
+        //JPQL기술필요
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    } //전체 조회
+
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    public long count() {
+        return em.createQuery("select count(m) from Member m", Long.class)
+                .getSingleResult(); //하나만 반환해줌.
     }
 
     public Member find(Long id) {
