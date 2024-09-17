c77e476a5d0abce99b16d14011b3cb6c2dbbc64a
0gonge <Challengef0802@gmail.com>
2024-09-18 01:10:59 +0900

    Interface로 migrate

--- src/main/java/study/example/data_jpa/repository/MemberRepository.java
+++ src/main/java/study/example/data_jpa/repository/MemberRepository.java
@@ -3,6 +3,9 @@ package study.example.data_jpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.example.data_jpa.entity.Member;
 
-public interface MemberRepository extends JpaRepository <Member, Long> {
+import java.util.List;
 
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 }
+
